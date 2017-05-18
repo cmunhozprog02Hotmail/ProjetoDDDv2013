db74ad37ada0bfe5c29a45bb750581dbc21ec4e7
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-05-18 00:27:25 -0300

    Instalação do EntityFramework Infra.Data 33:09

---
+++ ProjetoDDDv2013/ProjetoDDDv2013.Domain/Entities/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoDDDv2013.Domain.Entities
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime DataCadasto { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public bool ClienteEspecial(Cliente cliente)
+        {
+            return cliente.Ativo && DateTime.Now.Year - cliente.DataCadasto.Year >= 5;
+        }
+    }
+}
